36061e546945e0a1acae30eb1c96d0c9f421af7f
Erik Kieckhafer <ek@ato.la>
2020-02-26 11:21:39 -0800

    add health check code
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
@@ -14,7 +15,23 @@ class AuthenticatedDataSource extends RemoteGraphQLDataSource {
     if (context.authorizationToken) {
       request.http.headers.set('Authorization', context.authorizationToken);
     }
+
+    // figure out what it means to be a 2nd-3rd-4th request
+    // do we need to call super?
+
+    // check user permission before doing anything in the gateway
+    console.log(" ----- ----- ----- we are checking permissions here");
+    const isAuthorized = false;
+    if (!isAuthorized) {
+      console.log(" --------------------------------- inside the if block");
+
+      throw Error("oh snap an error");
+
+    }
+    console.log(" --------------------------- post checking permissions");
+
   }
+
 }
 
 async function main () {
@@ -23,9 +40,18 @@ async function main () {
     console.warn("No services registered.");
   }
 
+  const response = await axios.get("http://api.reaction.localhost:3000/.well-known/apollo/server-health");
+
+  console.log(" -------- axios response", response);
+
+
   const gateway = new ApolloGateway({
     serviceList: config.SERVICES,
     buildService({ url }) {
+      // check health status of
+      // do somethign with Map.set here?
+      // /curl --head http://localhost:3000/.well-known/apollo/server-health
+
       return new AuthenticatedDataSource({ url });
     },
     debug: true
