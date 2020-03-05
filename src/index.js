f801cb048f67ab52f0d1f88dfb1103fb0d81e9be
Erik Kieckhafer <ek@ato.la>
2020-03-04 19:30:38 -0800

    feature: add authorization service check inside willSendRequest
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/index.js
+++ src/index.js
@@ -4,11 +4,38 @@ const require = createRequire(import.meta.url);
 
 import config from "./config.js";
 
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, AuthenticationError } = require("apollo-server");
+const { createApolloFetch } = require("apollo-fetch");
 const { ApolloGateway, RemoteGraphQLDataSource } = require("@apollo/gateway");
 
+// check Gateway level permissions
+const checkAuthorization = async (route) => {
+  const fetch = createApolloFetch({
+    uri: config.AUTHORIZATION_URL
+  });
+
+  const { data: { isAuthorized } } = await fetch({
+    query: `query isAuthorized($action: String, $resource: String, $subject: String) {
+      isAuthorized(input: { action: $action, resource: $resource, subject: $subject })
+    }`,
+    variables: { action: "read", resource: `reaction:federation:route:${route}` },
+  });
+
+  return isAuthorized;
+}
+
 class AuthenticatedDataSource extends RemoteGraphQLDataSource {
-  willSendRequest({ request, context }) {
+  async willSendRequest({ request, context }) {
+    const route = request.http.url.replace(/(^\w+:|^)\/\//, '').split(".").splice(0, 1).join(); // TODO: this probably isn't the best way to do this
+
+    const isAuthorized = await checkAuthorization(route);
+
+    // if this was a user initiated request,
+    // the user must be authorized to use the Gateway
+    if (context.requestInitiated && !isAuthorized) {
+      throw new AuthenticationError("Gateway access denied");
+    }
+
     // pass the authorization token from the context to underlying services
     // as a header called "Authorization"
     if (context.authorizationToken) {
@@ -36,7 +63,13 @@ async function main () {
     subscriptions: false,
     context: ({ req }) => {
       const authorizationToken = req.headers.authorization || null;
-      return { authorizationToken };
+      return {
+        authorizationToken,
+        // requestInitiated tells `willSendRequest` that this was a user initiated request,
+        // not just a service request like startup or ping,
+        // and authorization / access needs to be checked
+        requestInitiated: true
+      };
     }
   });
 
