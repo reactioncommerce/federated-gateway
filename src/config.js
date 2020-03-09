9cbc226d385ea8f28320a2e41267bfc21ea65695
Erik Kieckhafer <ek@ato.la>
2020-03-09 12:56:21 -0700

    refactor: remove services array from .env
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ import envalid from "envalid";
 
 export default envalid.cleanEnv(process.env, {
   AUTHORIZATION_URL: envalid.str(),
-  GATEWAY_AUTHORIZATION_ENABLED: envalid.bool(),
-  SERVICES: envalid.json()
+  GATEWAY_AUTHORIZATION_ENABLED: envalid.bool()
 });
