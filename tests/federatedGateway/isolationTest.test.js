341d2e291b3e84f0a0968d7908c550b99683a476
Erik Kieckhafer <ek@ato.la>
2020-03-11 16:15:11 -0700

    tests: add test for single service in isolation
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

---
+++ tests/federatedGateway/isolationTest.test.js
@@ -0,0 +1,48 @@
+import { createRequire } from "module";
+import productService from "./services/products.js";
+
+const require = createRequire(import.meta.url);
+const { executeGraphql } = require("federation-testing-tool");
+const { gql } = require("apollo-server");
+
+describe("Based on the data from the external service", () => {
+  const query = gql`
+    {
+      _getProduct {
+        inStock
+        shippingEstimate
+      }
+    }
+  `;
+
+  it("should set the shippingEstimate at 0 for an expensive item and retrieve inStock", async () => {
+    const mocks = {
+      Product: () => ({
+        upc: "1",
+        weight: 10,
+        price: 14000,
+      })
+    };
+
+    const result = await executeGraphql({ query, mocks, service: productService });
+
+    expect(result.data._getProduct.shippingEstimate).toEqual(0);
+    expect(result.data._getProduct).toEqual({
+      inStock: true,
+      shippingEstimate: 0
+    });
+  });
+
+  it("should calculate the shipping estimate for cheap item", async () => {
+    const mocks = {
+      Product: () => ({
+        upc: "1",
+        weight: 10,
+        price: 10,
+      })
+    };
+
+    const result = await executeGraphql({ query, mocks, service: productService });
+    expect(result.data._getProduct.shippingEstimate).toEqual(5);
+  });
+});
