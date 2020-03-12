c2dec20c192dc7c40c16a3640c0e5223c944c809
Erik Kieckhafer <ek@ato.la>
2020-03-11 17:36:37 -0700

    tests: add combinedTest
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/services.js
+++ src/services.js
@@ -1,1 +1,7 @@
-export default [];
+export default [
+  {
+    "name": "api", "url": "http://api.reaction.localhost:3000/graphql"
+  }, {
+    "name": "authorization", "url": "http://authorization.reaction.localhost:6000"
+  }
+];
